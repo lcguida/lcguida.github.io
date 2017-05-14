d9841ccf1b78964fdc6c7af64e905edeef2deb8e
Leandro Guida <lcguida@gmail.com>
2017-05-14 18:47:43 +0200

    Fixes misplacement of task in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :rvm_ruby_version, '2.4.0@lcguida'
 
 namespace :deploy do
 
-  task: :jekyll_build do
+  task :jekyll_build do
     on roles(:app), in: :groups, limit: 3, wait: 10 do
       within current_path do
         execute :bundle, 'exec jekyll build'
@@ -20,7 +20,6 @@ namespace :deploy do
     end
   end
 
+  # Run the jekyll build command after the release folder is created
+  after "symlink:release", ":jekyll_buil"
 end
-
-# Run the jekyll build command after the release folder is created
-after "symlink:release", "update_jekyll"
